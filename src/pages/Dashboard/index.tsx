8d27f1113c53a58df30c99395adaf6ccf2222324
willfeliz1 <william_mfelizardo@hotmail.com>
2020-07-30 17:54:23 -0300

    feat: delete food selectedi and list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -58,7 +58,9 @@ const Dashboard: React.FC = () => {
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    api.delete(`/foods/${id}`).then(response => {
+      setFoods(foodState => foodState.filter(foodplate => foodplate.id !== id));
+    });
   }
 
   function toggleModal(): void {
